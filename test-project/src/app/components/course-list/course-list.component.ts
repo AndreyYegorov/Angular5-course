a0709a3d75a67c685f94ec8b41e785ab01264c03
andreyegorov <andrey.yegorov.mail@gmail.com>
2018-02-17 15:34:43 +0200

    updated CourseListComponent

--- test-project/src/app/components/course-list/course-list.component.ts
+++ test-project/src/app/components/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, ViewEncapsulation} from '@angular/core';
+import {CourseModel} from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-course-list',
@@ -7,5 +8,33 @@ import {Component, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent {
+  courses: CourseModel[] = [
+    {
+      id: 1,
+      topRated: false,
+      title: 'Course 1',
+      description: 'Some text for course 1',
+      duration: 360000,
+      createdAt: new Date()
+    },{
+      id: 2,
+      topRated: true,
+      title: 'Course 2',
+      description: 'Some text for course 2',
+      duration: 18000,
+      createdAt: new Date()
+    },{
+      id: 3,
+      topRated: false,
+      title: 'Course 3',
+      description: 'Some text for course 3',
+      duration: 520000,
+      createdAt: new Date()
+    }
+  ];
 
+  getTitle(title): string {
+    console.log('course title', title);
+    return title;
+  }
 }
